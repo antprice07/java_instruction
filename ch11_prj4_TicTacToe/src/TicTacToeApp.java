08e381590b6c0a9678630e665dfc036d25a166a2
Anthony Price <antprice07@gmail.com>
2019-10-10 23:41:23 -0400

    tic tac toe

--- ch11_prj4_TicTacToe/src/TicTacToeApp.java
+++ ch11_prj4_TicTacToe/src/TicTacToeApp.java
@@ -8,48 +8,146 @@ public class TicTacToeApp {
 		String p2 = " O ";
 		System.out.println("Welcome to Tic Tac Toe");
 		String[][] space = new String[3][3];
-		space[0][0]="   ";
-		space[0][1]="   ";
-		space[0][2]="   ";
-		space[1][0]="   ";
-		space[2][0]="   ";
-		space[1][1]="   ";
-		space[1][2]="   ";
-		space[2][1]="   ";
-		space[2][2]="   ";
-		int[][] selections = new int[3][3];
-		selections[0][0]=1;
-		
-		while(!gameOver) {
+		space[0][0] = "   ";
+		space[0][1] = "   ";
+		space[0][2] = "   ";
+		space[1][0] = "   ";
+		space[2][0] = "   ";
+		space[1][1] = "   ";
+		space[1][2] = "   ";
+		space[2][1] = "   ";
+		space[2][2] = "   ";
+
+		do{
 			gridPrint(space);
-			int row = Console.getInt("X's turn\r\n" + 
-					"Pick a row (1, 2, 3): ");
-			int column = Console.getInt("Pick a row (1, 2, 3): ");
-			concat(row,column);
-			
-			
-			
+			int selection = choice(p1);
+			fillSpace(selection,space,p1);
+			gameOver = gameOverCheck(space,p1);
+			gridPrint(space);
+			selection = choice(p2);
+			fillSpace(selection,space,p2);
+			gameOver = gameOverCheck(space,p2);
+
+		}while (!gameOver);
+		System.out.println("Game Over. :)");
+	}
+
+	public static void gridPrint(String[][] space) {
+		System.out.println("+---+---+---+");
+		System.out.println("|" + space[0][0] + "|" + space[0][1] + "|" + space[0][2] + "|");
+		System.out.println("+---+---+---+");
+		System.out.println("|" + space[1][0] + "|" + space[1][1] + "|" + space[1][2] + "|");
+		System.out.println("+---+---+---+");
+		System.out.println("|" + space[2][0] + "|" + space[2][1] + "|" + space[2][2] + "|");
+		System.out.println("+---+---+---+");
+
+	}
+
+	static int concat(int a, int b) {
+		String s1 = Integer.toString(a);
+		String s2 = Integer.toString(b);
+		s1 += s2;
+		int c = Integer.parseInt(s1);
+		return c;
+	}
+
+	static int choice(String player) {
+		int row = Console.getInt(player + "'s turn\r\n" + "Pick a row (1, 2, 3): ",0,4);
+		int column = Console.getInt("Pick a column (1, 2, 3): ",0,4);
+		int selection = concat(row, column);
+		return selection;
+	}
+
+	static void fillSpace(int selection, String[][] choices, String player) {
+		if (selection == 11) {
+			choices[0][0] = player;
 		}
+		if(selection == 12) {
+			choices[0][1] = player;
+		}
+		if(selection == 13) {
+			choices[0][2] = player;
+		}
+		if(selection == 21) {
+			choices[1][0] = player;
+		}
+		if(selection == 22) {
+			choices[1][1] = player;
+		}
+		if(selection == 23) {
+			choices[1][2] = player;
+		}
+		if(selection == 31) {
+			choices[2][0] = player;
+		}
+		if(selection == 32) {
+			choices[2][1] = player;
+		}
+		if(selection == 33) {
+			choices[2][2] = player;
+		}
+	}
+	static boolean gameOverCheck(String[][]space,String player) {
+		boolean gameOver = false;
+		if(space[0][0].equalsIgnoreCase(player) && space[0][1].equalsIgnoreCase(player)&&space[0][2].equalsIgnoreCase(player)) {
+			gameOver=true;
+			return gameOver;
+		}
+		if(space[0][0].equalsIgnoreCase(player)&& space[1][0].equalsIgnoreCase(player)&&space[2][0].equalsIgnoreCase(player)) {
+			gameOver=true;
+			return gameOver;
+		}
+		if(space[0][1].equalsIgnoreCase(player)&& space[1][1].equalsIgnoreCase(player)&&space[2][1].equalsIgnoreCase(player)) {
+			gameOver=true;
+			return gameOver;
+		}
+		if(space[0][2].equalsIgnoreCase(player)&& space[1][2].equalsIgnoreCase(player)&&space[2][2].equalsIgnoreCase(player)) {
+			gameOver=true;
+			return gameOver;
+		}
+		if(space[1][0].equalsIgnoreCase(player)&& space[1][1].equalsIgnoreCase(player)&&space[1][2].equalsIgnoreCase(player)) {
+			gameOver=true;
+			return gameOver;
+		}
+		if(space[2][0].equalsIgnoreCase(player)&& space[2][1].equalsIgnoreCase(player)&&space[2][2].equalsIgnoreCase(player)) {
+			gameOver=true;
+			return gameOver;
+		}
+		if(space[0][0].equalsIgnoreCase(player)&& space[1][1].equalsIgnoreCase(player)&&space[2][2].equalsIgnoreCase(player)) {
+			gameOver=true;
+			return gameOver;
+		}
+		if(space[0][2].equalsIgnoreCase(player)&& space[1][1].equalsIgnoreCase(player)&&space[2][0].equalsIgnoreCase(player)) {
+			gameOver=true;
+			return gameOver;
+		}else
+			return gameOver;
 		
 	}
 	
-	public static void gridPrint(String[][] space) {
-		System.out.println("+---+---+---+");
-		System.out.println("|"+space[0][0]+"|"+space[0][1]+"|"+space[0][2]+"|");
-		System.out.println("+---+---+---+");
-		System.out.println("|"+space[1][0]+"|"+space[1][1]+"|"+space[1][2]+"|");
-		System.out.println("+---+---+---+");
-		System.out.println("|"+space[2][0]+"|"+space[2][1]+"|"+space[2][2]+"|");
-		System.out.println("+---+---+---+");
-		
-	}
-	static int concat(int a, int b) 
-	{
-	    String s1 = Integer.toString(a); 
-	    String s2 = Integer.toString(b);  
-	    s1+=s2;
-	    int c = Integer.parseInt(s1);
-	    return c; 
-	}
-
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
