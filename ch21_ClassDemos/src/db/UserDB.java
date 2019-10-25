6fa6af8bd8c2d45212612f183077fc0bac1975de
Anthony Price <antprice07@gmail.com>
2019-10-24 21:14:39 -0400

    added update and delete methods to prs console app

--- ch21_ClassDemos/src/db/UserDB.java
+++ ch21_ClassDemos/src/db/UserDB.java
@@ -17,24 +17,14 @@ public class UserDB {
 		return con;
 	}
 
+	// NOTE: uses statement instead of prepared statement
 	public List<User> list() throws SQLException {
 		Statement stmt = getConnection().createStatement();
 		String sql = "SELECT * FROM User";
 		List<User> users = new ArrayList<>();
 		ResultSet rs = stmt.executeQuery(sql);
 		while (rs.next()) {
-			// parse user row into user pojo
-			int id = rs.getInt("id");
-			String userName = rs.getString("Username");
-			String pwd = rs.getString("password");
-			String fname = rs.getString("firstname");
-			String lname = rs.getString("lastname");
-			String phone = rs.getString("phonenumber");
-			String email = rs.getString("email");
-			boolean rvw = rs.getBoolean("isreviewer");
-			boolean adm = rs.getBoolean("isadmin");
-
-			User u = new User(id, userName, pwd, fname, lname, phone, email, rvw, adm);
+			User u = createUserFromRS(rs);
 
 			users.add(u);
 
@@ -44,65 +34,96 @@ public class UserDB {
 
 	}
 
+	// NOTE: uses statement instead of prepared statement
 	public User get(int id) throws SQLException {
 		Statement stmt = getConnection().createStatement();
 		String sql = "SELECT * FROM User" + " WHERE ID = " + id;
 		User u = null;
 		ResultSet rs = stmt.executeQuery(sql);
 		if (rs.next()) {
-			// parse user row into user pojo
-			String userName = rs.getString("Username");
-			String pwd = rs.getString("password");
-			String fname = rs.getString("firstname");
-			String lname = rs.getString("lastname");
-			String phone = rs.getString("phonenumber");
-			String email = rs.getString("email");
-			boolean rvw = rs.getBoolean("isreviewer");
-			boolean adm = rs.getBoolean("isadmin");
-
-			u = new User(id, userName, pwd, fname, lname, phone, email, rvw, adm);
-
+			u = createUserFromRS(rs);
 		}
 		rs.close();
 		return u;
 	}
-	
-	public void add() throws SQLException {
+
+	// NOTE: uses statement instead of prepared statement
+	public void addUser() throws SQLException {
 		Statement stmt = getConnection().createStatement();
 		System.out.println("ADD USER");
-		int id = Console.getInt("ID: ");
-		User u = get(id);
-		if(u == null) {
-			String sql = "INSERT INTO user (ID, UserName, Password, FirstName, LastName, PhoneNumber, Email, IsReviewer, IsAdmin) VALUES (";
-			String uname = Console.getString("Username: ");
-			String pwd = Console.getString("Password: ");
-			String fname = Console.getString("First Name: ");
-			String lname = Console.getString("Last Name: ");
-			String phone = Console.getString("Phone number(with dashes):");
-			String email = Console.getString("Email: ");
-			int rvw = Console.getInt("Is this user a reviewer? (1 for yes, 0 for no): ",-1,2);
-			int admin = Console.getInt("Is this user an admin? (1 for yes, 0 for no): ",-1,2);
-			int update = stmt.executeUpdate(sql+id+",'"+uname+"','"+pwd+"','"+fname+"','"+lname+"','"+phone+"','"+email+"',"+rvw+","+admin+")");
-			System.out.println("User added.");
-		}else {
-			System.out.println("Na fam. That is already an ID.");
+		String sql = "INSERT INTO user (UserName, Password, FirstName, LastName, PhoneNumber, Email, IsReviewer, IsAdmin) VALUES (";
+		String uname = Console.getString("Username: ");
+		String pwd = Console.getString("Password: ");
+		String fname = Console.getString("First Name: ");
+		String lname = Console.getString("Last Name: ");
+		String phone = Console.getString("Phone number(with dashes):");
+		String email = Console.getString("Email: ");
+		int rvw = Console.getInt("Is this user a reviewer? (1 for yes, 0 for no): ", -1, 2);
+		int admin = Console.getInt("Is this user an admin? (1 for yes, 0 for no): ", -1, 2);
+		int rowCount = stmt.executeUpdate(sql + "'" + uname + "','" + pwd + "','" + fname + "','" + lname + "','"
+				+ phone + "','" + email + "'," + rvw + "," + admin + ")");
+		System.out.println("User added.");
+
+	}
+
+	public int updateUser(int id, String choice, String change) {
+		String sql = "UPDATE USER SET " + choice + "= ? WHERE id = " + id;
+		int rowCount = 0;
+		User checkUser = null;
+		try {
+			checkUser = get(id);
+		} catch (SQLException e1) {
+			e1.printStackTrace();
 		}
+		if (checkUser != null) {
+			try (PreparedStatement ps = getConnection().prepareStatement(sql)) {
+				ps.setString(1, change);
+				rowCount = ps.executeUpdate();
+
+			} catch (SQLException e) {
+
+				e.printStackTrace();
+			}
+		} else {
+			System.out.println("Does not exist");
+		}
+		return rowCount;
 	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	public int deleteUser(int id) {
+		String sql = "DELETE FROM USER WHERE ID = ?";
+		int rowCount = 0;
+		User checkUser = null;
+		try {
+			checkUser = get(id);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		if(checkUser!=null) {
+			try(PreparedStatement ps = getConnection().prepareStatement(sql)){
+				ps.setInt(1, id);
+				ps.executeUpdate();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return rowCount;
+	}
+
+	private User createUserFromRS(ResultSet rs) throws SQLException {
+		// parse user row into user pojo
+		int id = rs.getInt("id");
+		String userName = rs.getString("Username");
+		String pwd = rs.getString("password");
+		String fname = rs.getString("firstname");
+		String lname = rs.getString("lastname");
+		String phone = rs.getString("phonenumber");
+		String email = rs.getString("email");
+		boolean rvw = rs.getBoolean("isreviewer");
+		boolean adm = rs.getBoolean("isadmin");
+
+		User u = new User(id, userName, pwd, fname, lname, phone, email, rvw, adm);
+		return u;
+	}
+
 }
--- ch21_ClassDemos/src/ui/PRSConsoleApp.java
+++ ch21_ClassDemos/src/ui/PRSConsoleApp.java
@@ -41,16 +41,39 @@ public class PRSConsoleApp {
 				break;
 			case 3:
 				try {
-					udb.add();
+					udb.addUser();
 				} catch (SQLException e) {
 					e.printStackTrace();
 				}
 				break;
 			case 4:
 				//update
+				System.out.println("Update User:");
+				id = Console.getInt("What ID would you like to update? ");
+				String choice = Console.getString("Would you like to change Username, Password,\n"
+						+ "FirstName, LastName, PhoneNumber, or Email? ");
+				String change = Console.getString("What do you want to make "+choice+"? ");
+				int rowCount = udb.updateUser(id, choice,change);
+				System.out.println(rowCount + " rows changed.");
 				break;
 			case 5:
 				//delete
+				System.out.println("Delete a User:");
+				id = Console.getInt("What is the ID of the user you'd like to delete? ");
+				User u = null;
+				try {
+					u = udb.get(id);
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+				System.out.println(u);
+				choice = Console.getString("Are you sure you want to delete? This cannot be undone. y/n ");
+				if(choice.equalsIgnoreCase("y")) {
+					rowCount = udb.deleteUser(id);
+					System.out.println(rowCount + " rows deleted.");
+				}else {
+					System.out.println("Action cancelled.");
+				}
 				break;
 			case 99:
 				//exit
