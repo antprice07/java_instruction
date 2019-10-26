c989a20044de8a10f2de8c4c595355166d100610
Anthony Price <antprice07@gmail.com>
2019-10-25 20:13:49 -0400

    added productdb and vendordb

---
+++ ch21_ClassDemos/src/db/ProductDB.java
@@ -0,0 +1,130 @@
+package db;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import business.Product;
+
+public class ProductDB {
+
+	protected static Connection getConnection() throws SQLException {
+		String dbURL = "jdbc:mysql://localhost:3306/prs?allowPublicKeyRetrieval=true&useSSL=false";
+		String username = "prs_user";
+		String pwd = "sesame";
+		Connection con = DriverManager.getConnection(dbURL, username, pwd);
+		return con;
+	}
+
+	public List<Product> list() throws SQLException {
+		Statement stmt = getConnection().createStatement();
+		String sql = "SELECT * FROM Product";
+		List<Product> products = new ArrayList<>();
+		ResultSet rs = stmt.executeQuery(sql);
+		while (rs.next()) {
+			Product u = createProductFromRS(rs);
+
+			products.add(u);
+
+		}
+		rs.close();
+		return products;
+	}
+
+	public Product get(int id) {
+		Product p = null;
+		String sql = "Select * from Product WHERE ID = ?";
+		try (PreparedStatement ps = getConnection().prepareStatement(sql)) {
+			ps.setInt(1, id);
+			ResultSet rs = ps.executeQuery();
+			if (rs.next()) {
+				p = createProductFromRS(rs);
+				rs.close();
+			} else {
+				System.out.println("No Product found with that ID.");
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return p;
+	}
+	
+	public int addProduct(Product p) {
+		int rowCount = 0;
+		String sql = "INSERT INTO Product (vendorID,partnumber,name,price,unit,photopath) VALUES(?,?,?,?,?,?)";
+		try(PreparedStatement ps = getConnection().prepareStatement(sql)){
+			ps.setInt(1, p.getVendorID());
+			ps.setString(2, p.getPartNumber());
+			ps.setString(3, p.getName());
+			ps.setDouble(4, p.getPrice());
+			ps.setString(5, p.getUnit());
+			ps.setString(6, p.getPhotoPath());
+			rowCount = ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return rowCount;
+	}
+	
+	public int updateProduct(int id, String choice,String change) {
+		int rowCount = 0;
+		if (!choice.contains("'")) {
+			String sql = "UPDATE PRODUCT SET "+choice+"= ? WHERE id = ?";
+			Product productCheck = get(id);
+			if (productCheck!=null) {
+				try(PreparedStatement ps = getConnection().prepareStatement(sql)){
+					ps.setString(1, change);
+					ps.setInt(2, id);
+					rowCount=ps.executeUpdate();
+				}catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}else {
+				System.out.println("No Product found with that ID.");
+			}
+		}else {
+			System.out.println("You can not use a single quote in your selection.");
+		}
+		
+		return rowCount;
+	}
+	
+	public int deleteProduct(int id) {
+		String sql = "DELETE FROM PRODUCT WHERE ID = ?";
+		int rowCount = 0;
+		Product checkProduct = null;
+		checkProduct = get(id);
+		if(checkProduct!=null) {
+			try(PreparedStatement ps = getConnection().prepareStatement(sql)){
+				ps.setInt(1, id);
+				ps.executeUpdate();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return rowCount;
+	}
+
+	private Product createProductFromRS(ResultSet rs) throws SQLException {
+		// parse user row into product pojo
+		int id = rs.getInt("id");
+		int vID = rs.getInt("VendorID");
+		String partNumber = rs.getString("partNumber");
+		String name = rs.getString("name");
+		double price = rs.getDouble("price");
+		String unit = rs.getString("unit");
+		String photoPath = rs.getString("photoPath");
+
+		Product p = new Product(id, vID, partNumber, name, price, unit, photoPath);
+		return p;
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ ch21_ClassDemos/src/db/VendorDB.java
@@ -0,0 +1,123 @@
+package db;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import business.Product;
+import business.Vendor;
+
+public class VendorDB {
+
+	public List<Vendor> list() throws SQLException {
+		Statement stmt = ProductDB.getConnection().createStatement();
+		String sql = "SELECT * FROM Vendor";
+		List<Vendor> vendors = new ArrayList<>();
+		ResultSet rs = stmt.executeQuery(sql);
+		while (rs.next()) {
+			Vendor v = createVendorFromRS(rs);
+
+			vendors.add(v);
+
+		}
+		rs.close();
+		return vendors;
+	}
+
+	public Vendor get(int id) {
+		Vendor v = null;
+		String sql = "Select * from Vendor WHERE ID = ?";
+		try (PreparedStatement ps = ProductDB.getConnection().prepareStatement(sql)) {
+			ps.setInt(1, id);
+			ResultSet rs = ps.executeQuery();
+			if (rs.next()) {
+				v = createVendorFromRS(rs);
+				rs.close();
+			} else {
+				System.out.println("No Vendor found with that ID.");
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return v;
+	}
+	
+	public int addVendor(Vendor v) {
+		int rowCount = 0;
+		String sql = "INSERT INTO Vendor (code,name,address,city,state,zip,phonenumber,email) VALUES(?,?,?,?,?,?,?,?)";
+		try(PreparedStatement ps = ProductDB.getConnection().prepareStatement(sql)){
+			ps.setString(1, v.getCode());
+			ps.setString(2, v.getName());
+			ps.setString(3, v.getAddress());
+			ps.setString(4, v.getCity());
+			ps.setString(5, v.getState());
+			ps.setString(6, v.getZip());
+			ps.setString(7, v.getPhoneNumber());
+			ps.setString(8, v.getEmail());
+			rowCount = ps.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return rowCount;
+	}
+	
+	public int updateVendor(int id, String choice,String change) {
+		int rowCount = 0;
+		if (!choice.contains("'")) {
+			String sql = "UPDATE Vendor SET "+choice+"= ? WHERE id = ?";
+			Vendor vendorCheck = get(id);
+			if (vendorCheck!=null) {
+				try(PreparedStatement ps = ProductDB.getConnection().prepareStatement(sql)){
+					ps.setString(1, change);
+					ps.setInt(2, id);
+					rowCount=ps.executeUpdate();
+				}catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}else {
+				System.out.println("No Product found with that ID.");
+			}
+		}else {
+			System.out.println("You can not use a single quote in your selection.");
+		}
+		
+		return rowCount;
+	}
+	
+	public int deleteVendor(int id) {
+		String sql = "DELETE FROM Vendor WHERE ID = ?";
+		int rowCount = 0;
+		Vendor checkVendor = null;
+		checkVendor = get(id);
+		if(checkVendor!=null) {
+			try(PreparedStatement ps = ProductDB.getConnection().prepareStatement(sql)){
+				ps.setInt(1, id);
+				ps.executeUpdate();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return rowCount;
+	}
+
+	private Vendor createVendorFromRS(ResultSet rs) throws SQLException {
+		// parse user row into product pojo
+		int id = rs.getInt("id");
+		String code = rs.getString("code");
+		String name = rs.getString("name");
+		String address = rs.getString("address");
+		String city = rs.getString("city");
+		String state = rs.getString("state");
+		String zip = rs.getString("zip");
+		String phone = rs.getString("phonenumber");
+		String email = rs.getString("email");
+
+		Vendor v = new Vendor(id, code, name, address, city, state, zip,phone,email);
+		return v;
+	}
+}
