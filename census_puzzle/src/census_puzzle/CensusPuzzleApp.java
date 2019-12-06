c929d57d257b3f71a4743db738fa1b410008c58f
Anthony Price <antprice07@gmail.com>
2019-12-05 21:25:48 -0500

    census puzzle

---
+++ census_puzzle/src/census_puzzle/CensusPuzzleApp.java
@@ -0,0 +1,26 @@
+package census_puzzle;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CensusPuzzleApp {
+
+	public static void main(String[] args) {
+		
+		List<Integer> cityPopulations = new ArrayList<>();
+		
+		cityPopulations.addAll(Arrays.asList(18897109, 12828837, 9461105, 6371773, 5965343, 5946800, 5582170, 5564635, 5268860, 4552402, 4335391, 4296250, 4224851, 4192887, 3439809, 3279833, 3095313, 2812896, 2783243, 2710489, 2543482, 2356285, 2226009, 2149127, 2142508, 2134411));
+		
+		int totalPop = 0;
+		
+		while(totalPop!=1000000000) {
+			for(int i=0;i<cityPopulations.size();i++) {
+				int total = 0;
+				for()
+			}
+		}
+		
+	}
+
+}
